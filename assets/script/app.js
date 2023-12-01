47ce96ecca427c574641fce01cf23358468012f3
Theodore <teddyakpati@gmail.com>
2023-12-01 13:57:31 -0600

    Added reset css file

---
+++ assets/script/app.js
@@ -0,0 +1,31 @@
+import { onEvent, select, selectAll, create, print } from "utils.js"
+
+import { Score } from "./score.js";
+
+const words = [
+  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+  'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
+  'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
+  'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
+  'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
+  'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
+  'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
+  'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
+  'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
+  'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+  'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
+  'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
+  'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
+  'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
+];
+
+const sound = new Audio();
+sound.type = '';
+
+const start = select('.start');
+const time = select('.time');
+const info = select('.info');
