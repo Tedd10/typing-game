d1220e061a8903345e43d8924f3f2c6e6ebb9357
Theodore <teddyakpati@gmail.com>
2023-12-01 13:42:16 -0600

    Added score and utils js files

---
+++ assets/script/score.js
@@ -0,0 +1,27 @@
+'use strict';
+
+export default 'Score';
+
+export class Score {
+  #data;
+  #hits;
+  #percentage;
+
+  constructor(data, hits, percentage) {
+    this.#data = data;
+    this.#hits = hits;
+    this.#percentage = percentage;
+  }
+
+  get data() {
+    return this.#data;
+  }
+
+  get hits() {
+    return this.#hits;
+  }
+
+  get percentage() {
+    return this.#percentage;
+  }
+}
